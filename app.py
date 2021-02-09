f2f77d7efc5b23de08a393d36435da1562f08a1c
Sumesh26 <sumesh.6361@gmail.com>
2021-02-09 23:19:01 +0530

    Code cleanup, added new endpoints for admin

--- app.py
+++ app.py
@@ -1,180 +1,332 @@
 import json
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify,session
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify,session,abort
 from flask_moment import Moment
 from models import *
 from flask_migrate import Migrate
-import requests
+from flask_cors import CORS,cross_origin
 import logging
 from functools import wraps
 from werkzeug.exceptions import HTTPException
-
+from datetime import datetime
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
+from helper_methods import *
+from forms import ReviewForm
+from auth import *
 
-app = Flask(__name__)
-moment = Moment(app)
+AUTH0_URL ='https://sumesh-fsnd.jp.auth0.com/authorize?audience=http://localhost:5000&response_type=token&client_id=5PrFVO8YBGSHJbF6XdvYjFyJ2N0VVj3z&redirect_uri=http://localhost:5000/home'
 
+def create_app(test_config=None):
+    app = Flask(__name__)
+    moment = Moment(app)
 
-app.config.from_object('config')
+    CORS(app, resources={r"*": {"origins": "*"}})
+    app.config.from_object('config')
 
-db.init_app(app)
-migrate = Migrate(app,db)
+    db.init_app(app)
+    migrate = Migrate(app,db)
 
-oauth = OAuth(app)
-auth0 = oauth.register(
-    'auth0',
-    client_id='5PrFVO8YBGSHJbF6XdvYjFyJ2N0VVj3z',
-    client_secret='A9SEQvAG_QdnVVnFal00FT7-5MSKZf4z4INPZn6TLeoTmvsL6rJLoVKXWvjer4bv',
-    api_base_url='https://sumesh-fsnd.jp.auth0.com',
-    access_token_url='https://sumesh-fsnd.jp.auth0.com/oauth/token',
-    authorize_url='https://sumesh-fsnd.jp.auth0.com/authorize',
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
-)
+    oauth = OAuth(app)
+    auth0 = oauth.register(
+        'auth0',
+        client_id='5PrFVO8YBGSHJbF6XdvYjFyJ2N0VVj3z',
+        client_secret='A9SEQvAG_QdnVVnFal00FT7-5MSKZf4z4INPZn6TLeoTmvsL6rJLoVKXWvjer4bv',
+        api_base_url='https://sumesh-fsnd.jp.auth0.com',
+        access_token_url='https://sumesh-fsnd.jp.auth0.com/oauth/token',
+        authorize_url='https://sumesh-fsnd.jp.auth0.com/authorize',
+        audience='http://localhost:5000',
+        client_kwargs={
+            'scope': 'openid profile email',
 
-
-def search_books_api(query):
-    response = requests.get('https://www.googleapis.com/books/v1/volumes?q={}'.format(query))
+        },
     
-    items=json.loads(response.text)['items']
-    
-    result=[]
-    
-    for item in items:
-        print('#'*100)
-      
-        author_list=item.get('volumeInfo',{}).get('authors')
-        
-        authors=''
-        if author_list != None:
-            for i,author in enumerate(author_list):
-                if i>0:
-                    authors+=', '
-                authors+=author
+    )
+    def requires_auth(permission=''):
+        def requires_auth_decorator(f):
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                if 'profile' not  in session:
+                    abort(401)
+                token = request.cookies.get('token')
+                print('token',token)
+                payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+                return f(payload, *args, **kwargs)
+
+            return wrapper
+        return requires_auth_decorator
             
-        category_list=item.get('volumeInfo',{}).get('categories')
-        categories=''
+    @app.route('/')
+    @app.route('/home')
+    def home():
+        reviews = []
+        if session.get('profile'):
+            name = session.get('profile')['name']
+        else:
+            name='Login'
+        return render_template('home.html',name=name)
+
+    @app.route('/admin/')
+    @requires_auth(permission='get:admin')
+    def admin_index(payload):
+        return render_template('admin/index.html')
+
+
+    @app.route('/reviews')
+    @requires_auth(permission='get:user_reviews')
+    def get_user_reviews(payload):
+        reviews = []
+        if session.get('profile'):
+            name = session.get('profile')['name']
+            id = session.get('profile')['user_id'][session.get('profile')['user_id'].index('|')+1:]
+            print(id)
+            reviews = User.query.filter(User.id==id).first().reviews
+        else:
+            name='Login'
+        return render_template('user_reviews.html',name=name,reviews=reviews)
+
+    @app.route('/search')
+    def search_books():
+        query_string=request.args.get('query_string')
+        if query_string == '' or query_string == None :
+            flash('Please enter the data in the search field')
+            items=[]
+        else:
+            items=search_books_api(query_string)
+        if session.get('profile'):
+            name = session.get('profile')['name']
+        else:
+            name='Login'
+        print(name)
         
-        if category_list != None:
-            for i,category in enumerate(category_list):
-                if i>0:
-                    categories+=', '
-                categories+=category
+        return render_template('book_search.html',items=items,name=name)
+
+
+    @app.route('/book/<string:id>')
+    def book_details(id):
+        book = Book.query.get(id)
+        #print([b.id for b in book if id==b.id])
+        
+        if book is None :
+            try:
+                book = get_book_details(id)
+                db.session.add(book)
+                db.session.commit()
+            except Exception as e:
+                flash('Error fetching the book')
                 
-        description=item.get('volumeInfo','No description').get('description','No description')
+                db.session.rollback()
+                print('eception: ',e)
+                abort(404)
+            
+        
+        if session.get('profile'):
+            name = session.get('profile')['name']
+        else:
+            name='Login'
+        if book is None:
+            abort(404)
+        reviews = book.reviews
+        posted=False
+        my_review = None
+        book_id=None
+        if session.get('profile'):
+            user_id = session.get('profile')['user_id'][session.get('profile')['user_id'].index('|')+1:]
+            user = User.query.filter(User.id==user_id).first()
+            
+            for review in reviews:
+                if user == review.user:
+                    posted=True
+                    my_review = review
+                    
+            
+        form = ReviewForm()
+        if posted:
+            form.title.data=my_review.title
+            form.review.data=my_review.comment
+            form.rating.data=my_review.rating
+            book_id = book.id
+            print(book_id)
+            
+        # db.session.close()
+        return render_template('book_detail.html',book_id=book_id,my_review=my_review,posted=posted,reviews=reviews,id=id,name=name,title=book.title,author=book.author,category=book.category,image_link=book.image_link,summary=book.summary,form=form)
+            
+    @app.route('/admin/reviews')
+    @requires_auth(permission='get:all_reviews')
+    def admin_fetch_all_reviews(payload):
+        if session.get('profile'):
+            name = session.get('profile')['name']
+        else:
+            abort(401)
+        reviews = Review.query.all()
+
+        return render_template('admin/reviews.html',reviews=reviews)
+
+    @app.route('/callback')
+    def callback_handling():
+        # Handles response from token endpoint
+        tk=auth0.authorize_access_token()
+        print('resp',tk)
+        resp = auth0.get('userinfo')
+        userinfo = resp.json()
+
        
-        description=description[:100]+'...'
+        # Store the user information in flask session.
+        session['jwt_payload'] = userinfo
+        print('uinfo',userinfo)
+        session['profile'] = {
+            'user_id': userinfo['sub'],
+            'name': userinfo['name'],
+            'picture': userinfo['picture']
+        }
+        flash('Welcome '+userinfo['name'])
+        user_id=(session.get('profile')['user_id'][session.get('profile')['user_id'].index('|')+1:])
+        if not User.query.filter(User.id==user_id ).first():
+            user = User(id =  user_id ,username=session['profile']['name'])
+            db.session.add(user)
+            db.session.commit()
+
+        return redirect(AUTH0_URL)
+
+
+    @app.route('/login')
+    def login():
+        return auth0.authorize_redirect(redirect_uri='http://localhost:5000/callback')
+
+    @app.route('/logout')
+    @requires_auth()
+    def logout(payload):
+        # Clear session stored data
+        session.clear()
+        # Redirect user to logout endpoint
+        params = {'returnTo': url_for('home', _external=True), 'client_id': '5PrFVO8YBGSHJbF6XdvYjFyJ2N0VVj3z'}
+        flash('Logout Successful')
+        return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+
+    @app.route('/profile')
+    @requires_auth()
+    def dashboard(payload):
+        return render_template('profile.html', 
+                                ck= request.cookies.get('token'),
+                            name = session['profile']['name'],
+                            userinfo=session['profile'],
+                            userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+
+
+    @app.route('/book/reviews/<string:book_id>',methods=['POST'])
+    @requires_auth(permission='post:user_reviews')
+    def post_review(payload,book_id):
+        try:
+            print('id =',book_id)
+            print(request.form['title'])
+            review = Review()
+            review.title = request.form['title']
+            review.comment = request.form['review']
+            review.rating = request.form['rating']
+
+            review.created = datetime.today()
+            review.edited = datetime.today()
+            print(review)
+            review.user_id = session.get('profile')['user_id'][session.get('profile')['user_id'].index('|')+1:]
+            review.book_id = book_id
+        
+            db.session.add(review)
+            db.session.commit()
+            print('success')
+        except Exception as e:
+            db.session.rollback()
+            flash('Error: Review could not be posted')
+            print('error',e)
+        finally:
+            db.session.close()
             
-        image_link=item.get('volumeInfo',{}).get('imageLinks',{}).get('smallThumbnail')
-        
-        title=item.get('volumeInfo',{}).get('title')
-        id=item.get('id',None)
-        print(title,' ',authors,' ',categories)
-        print(image_link)
-        print(description)
-        print('#'*100)
-        result.append(Book(id,title,authors,categories,description,image_link))
-        
-    return result
+        return redirect(url_for('book_details',id=book_id))
 
-def get_book_details(id):
-    response = requests.get('https://www.googleapis.com/books/v1/volumes/{}'.format(id))
-    print('res=',response.text)
-    item = json.loads(response.text)
-    print('#'*100)
-    
-    author_list=item.get('volumeInfo',{}).get('authors')
-    
-    authors=''
-    if author_list != None:
-        for i,author in enumerate(author_list):
-            if i>0:
-                authors+=', '
-            authors+=author
         
-    category_list=item.get('volumeInfo',{}).get('categories')
-    categories=''
-    
-    if category_list != None:
-        for i,category in enumerate(category_list):
-            if i>0:
-                categories+=', '
-            categories+=category
+    @app.route('/books/<string:book_id>/reviews/<int:review_id>',methods=['DELETE'])
+    @requires_auth(permission='delete:user_reviews')
+    def delete_review(payload,book_id,review_id):
+        try:
+            review = Review.query.get(review_id)
             
-    description=item.get('volumeInfo','No description').get('description','No description')
-    
-    description=description[:100]+'...'
-        
-    image_link=item.get('volumeInfo',{}).get('imageLinks',{}).get('smallThumbnail')
-    
-    title=item.get('volumeInfo',{}).get('title')
-    id=item.get('id',None)
-    print(title,' ',authors,' ',categories)
-    print(image_link)
-    print(description)
+            db.session.delete(review)
+            db.session.commit()
+            flash('Delete successful')
+        except Exception as e:
+            flash('Delete was unsuccessful')
+            db.session.rollback()
+            
 
-    print('#'*100)
-    return Book(id,title,authors,categories,description,image_link)
-    
-    
-        
-@app.route('/')
-def index():
-    return render_template('book_search.html')
+        finally:
+            db.session.close()
+            
+        return jsonify({'success':True})
 
-@app.route('/search')
-def search_books():
-    query_string=request.args['query_string']
-    print(query_string,'-----------------')
-    items=search_books_api(query_string)
-    return render_template('book_search.html',items=items)
+    @app.route('/admin/reviews/<int:review_id>',methods=['DELETE'])
+    @requires_auth(permission='delete:all_reviews')
+    def admin_delete_review(payload,review_id):
+        try:
+            review = Review.query.get(review_id)
+            db.session.delete(review)
+            db.session.commit()
+            flash('Delete successful')
+        except Exception as e:
+            flash('Error: Delete failed')
+            db.session.rollback()
+            print('error',e)
+        finally:
+            db.session.close()
+            
+        return jsonify({'success':True})
+
+    @app.route('/books/<string:book_id>/reviews/<int:review_id>',methods=['POST'])
+    @requires_auth(permission="patch:user_reviews")
+    def update_review(payload,book_id,review_id):
+        try:
+            review = Review.query.get(review_id)
+            print(review)
+            review.title = request.form['title']
+            review.comment = request.form['review']
+            review.rating = request.form['rating']
+            review.edited = datetime.today()
+
+            db.session.add(review)
+            db.session.commit()
+            flash('Changes saved successful')
+        except Exception as e:
+            db.session.rollback()
+            flash('Saving changes failed')
+            print('error',e)
+
+        finally:
+            db.session.close()
+        return redirect(url_for('book_details',id=book_id))
 
 
-@app.route('/book/<string:id>')
-def book_details(id):
-    book = Book.query.filter(Book.id==id).one_or_none()
-    if book == None:
-        book = get_book_details(id)
-        db.session.add(book)
-        db.session.commit()
 
-    return jsonify(book.format())
-        
-        
-@app.route('/callback')
-def callback_handling():
-    # Handles response from token endpoint
-    auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
-    userinfo = resp.json()
 
-    # Store the user information in flask session.
-    session['jwt_payload'] = userinfo
-    session['profile'] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture']
-    }
-    return redirect('/dashboard')
+    @app.route('/admin/reviews',methods=['GET'])
+    @requires_auth(permission='get:all_reviews')
+    def admin_fetch_reviews(payload):            
+        reviews = Review.query.all()
+        return render_template('admin/reviews.html',reviews=reviews)
 
-@app.route('/login')
-def login():
-    return auth0.authorize_redirect(redirect_uri='http://localhost:5000/callback')
+    @app.route('/admin/users',methods=['GET'])
+    @requires_auth(permission='get:all_users')
+    def admin_fetch_users(payload):
+        users = User.query.all()
+        return render_template('admin/users.html',users=users)
 
-def requires_auth(f):
-  @wraps(f)
-  def decorated(*args, **kwargs):
-    if 'profile' not in session:
-      # Redirect to Login page here
-      return redirect('/')
-    return f(*args, **kwargs)
 
-  return decorated
+    @app.errorhandler(404)
+    def resource_not_found(error):
+        return render_template('errors/404.html')
 
-@app.route('/dashboard')
-@requires_auth
-def dashboard():
-    return render_template('dashboard.html',
-                           userinfo=session['profile'],
-                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+    @app.errorhandler(401)
+    def not_authenticated(auth_error):
+            return render_template('errors/401.html')
+
+    return app
+
