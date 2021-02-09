c7241d8270c83b9f724263766708c385040e1394
Sumesh26 <sumesh.6361@gmail.com>
2021-02-10 00:49:09 +0530

    minor fix

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import create_app
+from app import app
 from models import db
 
-app=create_app()
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 
